3920b43e1c3607122773b9df28272ceabbb9d403
Blagovest <blagovest_georgiev2002@abv.bg>
2023-08-10 18:57:34 +0300

    Added ER diagram

--- src/main/java/com/example/model/repository/UserRepository.java
+++ src/main/java/com/example/model/repository/UserRepository.java
@@ -2,9 +2,12 @@ package com.example.model.repository;
 
 import com.example.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
     Optional<User> findUserByEmail(String email);
     Optional<User> findUserByPhoneNumber(String phoneNumber);
